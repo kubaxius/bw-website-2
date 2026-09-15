337465e3ef78b1e8a6e26277dd3ccf0176de687a
Jakub Niedźwiedź <jakub.niedzwiedz@gmail.com>
2026-09-15 19:04:13 +0200

    feat: add my translation plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import astroI18n from "@kubaxius/astro-i18n-mini";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    astroI18n({
+      locales: ["en", "pl"],
+      defaultLocale: "en",
+      translations: {
+        directory: "src/i18n/translations",
+        onMissing: "fallback",
+        warnOnMissing: true,
+      },
+      routing: {
+        prefixDefaultLocale: true,
+        redirectToDefaultLocale: true,
+      },
+    }),
+  ],
+});
